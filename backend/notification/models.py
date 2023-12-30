712cee0d37fd065bf79b2eeccc9b55c738330673
spig0126 <spig0126@gmail.com>
2023-12-30 17:06:44 +0000

    ✨ Feat: delete old notifications when user's/team's notification goes over 50

--- backend/notification/models.py
+++ backend/notification/models.py
@@ -10,6 +10,24 @@ class Notification(models.Model):
      related_id = models.PositiveIntegerField()
      created_at = models.DateTimeField(auto_now_add=True, blank=True)
      is_read = models.BooleanField(default=False)
+     
+     def save(self, *args, **kwargs):
+          super().save(*args, **kwargs)
+
+          # Check if the user has more than 50 notifications
+          user_notifications = Notification.objects.filter(to_user=self.to_user)
+          notifications_count = user_notifications.count()
+
+          if notifications_count > 50:
+               # Calculate how many notifications to delete
+               notifications_to_delete = notifications_count - 50
+
+               # Get the oldest notifications to delete
+               oldest_notifications = user_notifications.order_by("created_at")[:notifications_to_delete]
+
+               # Delete the oldest notifications
+               oldest_notifications.delete()
+
 
 class TeamNotification(models.Model):
      type = models.CharField(max_length=30)
@@ -24,3 +42,17 @@ class TeamNotification(models.Model):
           if is_new:
                # alert team members with new notification by updating noti_unread_cnt
                TeamMembers.objects.filter(team=self.to_team).update(noti_unread_cnt=F('noti_unread_cnt') + 1)
+          
+          # Check if the user has more than 50 notifications
+          team_notifications = TeamNotification.objects.filter(to_team=self.to_team)
+          notifications_count = team_notifications.count()
+
+          if notifications_count > 50:
+               # Calculate how many notifications to delete
+               notifications_to_delete = notifications_count - 50
+
+               # Get the oldest notifications to delete
+               oldest_notifications = team_notifications.order_by("created_at")[:notifications_to_delete]
+
+               # Delete the oldest notifications
+               oldest_notifications.delete()
