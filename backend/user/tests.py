4a1e2ea3fbeef09a8a0d95e8e401055f4996c1af
spig0126 <spig0126@gmail.com>
2024-01-01 13:22:11 +0000

    ✅ Test: add test for custom token creation

--- backend/user/tests.py
+++ backend/user/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+import firebase_admin
+from firebase_admin import credentials, auth
+import requests
+import json
 
-# Create your tests here.
+class FirebaseTokenGenerationTestCase(TestCase):
+     def test_custom_token_authentication(self):
+          uid = "8jTawiVplrR1yGlqS4xTKPWEbuC2"
+
+          # Generate a custom ID token for the specified user
+          custom_token = auth.create_custom_token(uid)
+          
+          api_key = 'AIzaSyBVWm8Frug_qriTsokbIt3Ca2pVz7bkXe0'
+          
+          print(custom_token)
