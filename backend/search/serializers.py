9af350126fe105746ebd82f6a9c41500884207ce
spig0126 <spig0126@gmail.com>
2024-01-06 03:24:13 +0000

    ✨ Feat: change  searched user/team history list serializer format
    - before: didn't show history pk. showed user not searched_user (same to team)
    - after: change serializer so that it shows hitory pk. show searched_user/searched_team as nested represnetation

--- backend/search/serializers.py
+++ backend/search/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from .models import *
 from user.serializers import UserField
+from user.serializers import UserDetailSerializer
+from team.serializers import SearchedTeamDetailSerializer
 
 class UserSearchHistoryDetailSerializer(serializers.ModelSerializer):
      class Meta:
@@ -13,4 +15,22 @@ class TeamSearchHistoryDetailSerializer(serializers.ModelSerializer):
           model = TeamSearchHistory
           fields = '__all__'
    
-          
+class SearchedUserHistoryDetailSerializer(serializers.ModelSerializer):
+     searched_user = UserDetailSerializer()
+     
+     class Meta:
+          model = UserSearchHistory
+          fields = [
+               'id',
+               'searched_user'
+          ]
+     
+class SearchedTeamHistoryDetailSerializer(serializers.ModelSerializer):
+     searched_team = SearchedTeamDetailSerializer()
+     
+     class Meta:
+          model = UserSearchHistory
+          fields = [
+               'id',
+               'searched_team'
+          ]
--- backend/search/views.py
+++ backend/search/views.py
@@ -34,15 +34,14 @@ class UserSearchHistoryListAPIView(generics.ListAPIView):
      
      def get_queryset(self):
           return UserSearchHistory.objects.filter(user=self.request.user).order_by('-timestamp')
-
+ 
+ 
 class SearchedUserHistoryListAPIView(generics.ListAPIView):
-     serializer_class = UserDetailSerializer
+     serializer_class = SearchedUserHistoryDetailSerializer
      
      def get_queryset(self):
-          search_histories = UserSearchHistory.objects.filter(user=self.request.user).order_by('-timestamp')
-          searched_users = [search_history.user for search_history in search_histories]
-          return searched_users
- 
+          return UserSearchHistory.objects.filter(user=self.request.user).order_by('-timestamp')
+     
 class DeleteUserSearchHistoryAPIView(generics.DestroyAPIView):
      def get_queryset(self):
           return UserSearchHistory.objects.filter(user=self.request.user)
@@ -75,12 +74,10 @@ class TeamSearchHistoryListAPIView(generics.ListAPIView):
           return TeamSearchHistory.objects.filter(user=self.request.user).order_by('-timestamp')
 
 class SearchedTeamHistoryListAPIView(generics.ListAPIView):
-     serializer_class = SearchedTeamDetailSerializer
+     serializer_class = SearchedTeamHistoryDetailSerializer
      
      def get_queryset(self):
-          search_histories = TeamSearchHistory.objects.filter(user=self.request.user).order_by('-timestamp')
-          searched_teams = [search_history.searched_team for search_history in search_histories]
-          return searched_teams
+          return TeamSearchHistory.objects.filter(user=self.request.user).order_by('-timestamp')
      
 class DeleteTeamSearchHistoryAPIView(generics.DestroyAPIView):
      def get_queryset(self):
