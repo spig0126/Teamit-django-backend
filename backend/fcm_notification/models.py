107d0ae0605007878853b629b1873ae6661072ef
spig0126 <spig0126@gmail.com>
2024-01-11 06:35:12 +0000

    🗃️ Chore: remove token unique flag
    - before: device can have only one account because of unique flag
    - after: multiple accounts can login in one device (after removing unique flag)

--- backend/fcm_notification/models.py
+++ backend/fcm_notification/models.py
@@ -5,6 +5,6 @@ from user.models import User
 class Device(models.Model):
      id = models.AutoField(primary_key=True)
      user = models.ForeignKey(User, on_delete=models.CASCADE)
-     token = models.CharField(unique=True)
+     token = models.CharField()
      timestamp = models.DateTimeField(auto_now=True)
      
