ec39ce2f51f822883d4e380913042930494f5728
spig0126 <spig0126@gmail.com>
2024-02-16 23:15:17 +0900

    ⚰️ Refactor: remove dead code

--- backend/firebase_auth/exceptions.py
+++ backend/firebase_auth/exceptions.py
@@ -7,9 +7,9 @@ class UserNotFoundWithAuthToken(APIException):
      default_code = "user_not_found_with_auth_token"
      
 class NoAuthToken(APIException):
-    status_code = status.HTTP_401_UNAUTHORIZED
-    default_detail = "authentication token not provided"
-    default_code = "no_auth_token"
+     status_code = status.HTTP_401_UNAUTHORIZED
+     default_detail = "authentication token not provided"
+     default_code = "no_auth_token"
 
 class InvalidAuthTokenFormat(APIException):
      status_code = status.HTTP_400_BAD_REQUEST
@@ -22,11 +22,11 @@ class ExpiredAuthToken(APIException):
      default_code = "expired_token"
 
 class InvalidAuthToken(APIException):
-    status_code = status.HTTP_401_UNAUTHORIZED
-    default_detail = "invalid authentication token provided"
-    default_code = "invalid_token"
+     status_code = status.HTTP_401_UNAUTHORIZED
+     default_detail = "invalid authentication token provided"
+     default_code = "invalid_token"
 
 class FirebaseError(APIException):
-    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    default_detail = "user provided with the auth token is not a valid Firebase user, it has no Firebase UID"
-    default_code = "no_firebase_uid"
+     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+     default_detail = "user provided with the auth token is not a valid Firebase user, it has no Firebase UID"
+     default_code = "no_firebase_uid"
