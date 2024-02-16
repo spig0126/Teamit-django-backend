4a858406aef1d75ae52bb74281445b0014bfddb9
spig0126 <spig0126@gmail.com>
2024-02-16 23:10:53 +0900

    🗃️ Chore: add fields to team chat models
    - add 'last_msg' field to TeamChatRoom
    - add fields for unread msg cnt for chatroom and chatmessage
    - add properties for serializers

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -9,27 +9,69 @@ class PrivateChatRoom(models.Model):
      last_msg = models.CharField(max_length=255, default='')
      created_at = models.DateTimeField(auto_now_add=True)
      updated_at = models.DateTimeField(auto_now=True)
+     last_msg = models.CharField(max_length=255)
      participants = models.ManyToManyField(
           User, 
           through="PrivateChatParticipant",
           related_name="private_chat_rooms"
      )
-     
      class Meta:
           ordering = ['-updated_at']
+          
      
 class PrivateChatParticipant(models.Model):
      chatroom = models.ForeignKey(PrivateChatRoom, on_delete=models.CASCADE)
      chatroom_name = models.CharField(max_length=50)
      user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
-     # unread_cnt = models.PositiveIntegerField(default=0)
+     unread_cnt = models.PositiveIntegerField(default=0)
+     alarm_on = models.BooleanField(default=True)
+     is_online = models.BooleanField(default=False)
+     last_read_time = models.DateTimeField(auto_now=True)
+     
+     @property
+     def avatar(self):
+          return self.chatroom.participants.exclude(user=self.user).values('avatar').first().get('avatar', 'avatars/default.png')
+     
+     @property
+     def background(self):
+          return self.chatroom.participants.exclude(user=self.user).values('background').first().get('background', '')
+     
+     @property
+     def updated_at(self):
+          return self.chatroom.updated_at
+     
+     @property
+     def last_msg(self):
+          return self.chatroom.last_msg
 
 class PrivateMessage(models.Model):
      chatroom = models.ForeignKey(PrivateChatRoom, on_delete=models.CASCADE)
      sender = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
      content = models.CharField(max_length=255)
      timestamp = models.DateField(auto_now_add=True)
+     is_msg = models.BooleanField(default=True)
      
+     @property
+     def name(self):
+          if self.sender is None:
+               return '(알 수 없음)'
+          else:
+               return self.sender.name
+     
+     @property
+     def avatar(self):
+          if self.sender is None:
+               return 'avatars/default.png'
+          else:
+               return self.sender.avatar.url
+     
+     @property
+     def background(self):
+          if self.sender is None:
+               return ''
+          else:
+               return self.sender.background.url
+
      class Meta:
           ordering = ['-timestamp']
      
@@ -45,7 +87,11 @@ class InquiryChatRoom(models.Model):
      created_at = models.DateTimeField(auto_now_add=True)
      updated_at = models.DateTimeField(auto_now=True)
      inquirer = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name='inquiry_chat_rooms')
-     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='inquiry_chat_rooms')
+     team = models.ForeignKey(Team, blank=True, null=True, on_delete=models.CASCADE, related_name='inquiry_chat_rooms')
+     inquirer_unread_cnt = models.PositiveIntegerField(default=0)
+     responder_unread_cnt = models.PositiveIntegerField(default=0)
+     inquirer_alarm_on = models.BooleanField(default=True)
+     responder_alarm_on = models.BooleanField(default=True)
      
      class Meta:
           ordering = ['-updated_at']
@@ -65,9 +111,9 @@ class TeamChatRoom(models.Model):
      team = models.ForeignKey(Team, on_delete=models.CASCADE)
      name = models.CharField(max_length=50)
      background = models.CharField(default='0xff00FFD1', max_length=10)
-     last_msg = models.CharField(max_length=255, default='')
      created_at = models.DateTimeField(auto_now_add=True)
      updated_at = models.DateTimeField(auto_now=True)
+     last_msg = models.CharField(max_length=255)
      participants = models.ManyToManyField(
           User, 
           through="TeamChatParticipant"
@@ -78,18 +124,72 @@ class TeamChatParticipant(models.Model):
      chatroom = models.ForeignKey(TeamChatRoom, blank=True, null=True, on_delete=models.CASCADE)
      user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
      member = models.ForeignKey(TeamMembers, related_name="participants", blank=True, null=True, on_delete=models.SET_NULL)
+     is_online = models.BooleanField(default=False)
+     last_read_time = models.DateTimeField(auto_now=True)
+     unread_cnt = models.PositiveIntegerField(default=0)
+     alarm_on = models.BooleanField(default=True)
      
+     @property
+     def name(self):
+          if self.user is None:
+               return '(알 수 없음)'
+          return self.user.name
+     
+     @property 
+     def position(self):
+          if self.user is None or self.member is None:
+               return ''
+          else:
+               return self.member.position.name
      class Meta:
           constraints = [
                models.UniqueConstraint(fields=['chatroom', 'user'], name='unique_team_chat_participant')
           ]
 
 class TeamMessage(models.Model):
-     chatroom = models.ForeignKey(TeamChatRoom, blank=True, null=True, on_delete=models.CASCADE)
-     sender = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+     chatroom = models.ForeignKey(TeamChatRoom, blank=True, null=True, on_delete=models.CASCADE, related_name="messages")
+     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
+     member = models.ForeignKey(TeamMembers, blank=True, null=True, on_delete=models.SET_NULL)
      content = models.CharField(max_length=255)
-     timestamp = models.DateField(auto_now_add=True)
+     timestamp = models.DateTimeField(auto_now_add=True)
+     is_msg = models.BooleanField(default=True)
      
+     @property
+     def name(self):
+          if not self.is_msg:
+               return ''
+          elif self.user is None:
+               return '(알 수 없음)'
+          return self.user.name
+     
+     @property
+     def avatar(self):
+          if not self.is_msg:
+               return ''
+          elif self.user is None:
+               return 'avatars/default.png'
+          return self.user.avatar.url
+     
+     @property
+     def background(self):
+          if not self.is_msg:
+               return ''
+          elif self.user is None:
+               return ''
+          elif self.member is None:
+               return '0xff45474D'
+          else:
+               return self.member.background
+     
+     @property 
+     def position(self):
+          if not self.is_msg:
+               return ''
+          elif self.member is None or self.user is None:
+               return ''
+          else:
+               return self.member.position.name
+
      class Meta:
           ordering = ['-timestamp']
      
