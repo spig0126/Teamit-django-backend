bc7ebe8e7c47e2d3e8d68941d939185c46006c56
spig0126 <spig0126@gmail.com>
2024-02-19 00:46:02 +0900

    ♻️ Refactor: continue

--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -4,7 +4,7 @@ from .consumers import *
 
 websocket_urlpatterns = [
      path("ws/private-chat/<str:chatroom_id>/", PrivateChatConsumer.as_asgi()),
-     path("ws/team-chat/<str:chatroom_id>/", TeamChatConsumer.as_asgi()),
+     path("ws/team-chat/<str:chatroom_id>/", TeamChatConsumer2.as_asgi()),
      path("ws/chat-status/", ChatStatusConsumer.as_asgi()),
      path("ws/inquiry-chat/<str:chatroom_id>/", PrivateChatConsumer.as_asgi()),
 ]
