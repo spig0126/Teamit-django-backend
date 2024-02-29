286b4df9d637ce055fdfbe9767428a2b9355428b
spig0126 <spig0126@gmail.com>
2024-03-01 00:16:13 +0900

    ✨ Feat: chat search
    - create indexes for each chatroom type
    - create properties that is used in indexes
    - add 'search' command in status_consumer
    - refactor inquiry chatroom detail serializer

--- backend/user/client.py
+++ backend/user/client.py
@@ -8,15 +8,6 @@ def get_index(index_name='user_index'):
      index = client.init_index(index_name)
      return index
 
-def refresh_user_index():
-    users = User.objects.all()
-
-    # Convert User objects to dictionaries suitable for indexing
-    records = [{'objectID': str(user.pk), 'name': user.name, 'other_fields': user.other_fields} for user in users]
-
-    # Push the data to Algolia to update the index
-    index.save_objects(records)
-    
 def perform_search(query, **kwargs):
      index = get_index()
      results = index.search(query)
