3318bd686884595fe3730ac6a94166784f5fa203
spig0126 <spig0126@gmail.com>
2024-03-26 17:16:55 +0900

    ✨ 🔀 Feat: report user review/comment (merge develop -> feature/review)

--- backend/report/urls.py
+++ backend/report/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 
 from .views import *
 
 urlpatterns = [
-     path("", ReportUserOrTeamAPIView.as_view()),
+     path("users/", UserReportCreateAPIView.as_view()),
+     path("teams/", TeamReportCreateAPIView.as_view()),
+     path("teams/posts/", TeamPostReportCreateAPIView.as_view()),
+     path("teams/posts/comments/", TeamPostCommentReportCreateAPIView.as_view()),
+     path("reviews/", UserReviewReportCreateAPIView.as_view()),
+     path("reviews/comments/", UserReviewCommentReportCreateAPIView.as_view()),
 ]
