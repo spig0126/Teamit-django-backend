8fc01d4f02ddea8ebbecad4912b53d0892b31075
spig0126 <spig0126@gmail.com>
2024-03-26 17:16:55 +0900

    ✨ 🔀 Feat: report user review/comment (merge develop -> feature/review)

--- backend/report/views.py
+++ backend/report/views.py
@@ -1,79 +1,45 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import generics, status
-from rest_framework.response import Response
-from rest_framework.exceptions import PermissionDenied
-from django.db import transaction
+from rest_framework import generics
 
 from .models import *
 from .serializers import *
 from user.models import User
 from team.models import Team
 
-class ReportUserOrTeamAPIView(generics.CreateAPIView):
-     serializer_class = ReportDetailSerializer
+class ReportCreateAPIView(generics.CreateAPIView):
+     def perform_create(self, serializer):
+          user = self.request.user
+          report = serializer.save(reporter=user)
+          block = bool(self.request.query_params.get('block', ''))
+          if block:
+               reported_type = report._meta.get_field('reported').related_model
+               try:
+                    if reported_type == Team:
+                         user.blocked_teams.add(report.reported)
+                    elif reported_type in (TeamPost, TeamPostComment):
+                         user.blocked_users.add(report.reported.writer.user)
+                    elif reported_type == UserReview:
+                         user.blocked_users.add(report.reported.reviewer)
+                    elif reported_type == UserReviewComment:
+                         user.blocked_users.add(report.reported.review.reviewee)
+                    else:
+                         user.blocked_users.add(report.reported)
+               except AttributeError:
+                    pass
+               
+class UserReportCreateAPIView(ReportCreateAPIView):
+     serializer_class = UserReportDetailSerializer
+
+class TeamReportCreateAPIView(ReportCreateAPIView):
+     serializer_class = TeamReportDetailSerializer
+
+class TeamPostReportCreateAPIView(ReportCreateAPIView):
+     serializer_class = TeamPostReportDetailSerializer
+
+class TeamPostCommentReportCreateAPIView(ReportCreateAPIView):
+     serializer_class = TeamPostCommentReportDetailSerializer
      
-     @transaction.atomic
-     def create(self, request, *args, **kwargs):
-          user = request.user
-          
-          reported_type = request.data.get('reported_type', None)
-          block = request.data.pop('block', None)
-          
-          # handle exceptions and block team/user
-          if reported_type == 'team':
-               reported_team_pk = request.data.get('reported_team', None)
-               reported_team = get_object_or_404(Team, pk=reported_team_pk)
-               
-               # abort if user is team's creator
-               if user == reported_team.creator:
-                    raise PermissionDenied("User is not allowed to report this team. user is creator of team.")
-          
-               # block team 
-               if block:
-                    user.blocked_teams.add(reported_team)
-               
-          elif reported_type == 'user':
-               reported_user_pk = request.data.get('reported_user', None)
-               reported_user = get_object_or_404(User, pk=reported_user_pk)
-               
-               # abort if reported user is reporter
-               if user == reported_user:
-                    return Response({"detail": "user cannot report oneself"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-               
-               # block user
-               if block:
-                    user.blocked_users.add(reported_user)
-     
-          elif reported_type == 'team_post':
-               reported_team_post_pk = request.data.get('reported_team_post', None)
-               team_post = get_object_or_404(TeamPost, pk=reported_team_post_pk)
-               
-               # abort if reported writer is reporter
-               if user.pk == team_post.writer.user.pk:
-                    return Response({"detail": "user cannot report oneself"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-               
-               # block writer(user)
-               if block:
-                    user.blocked_users.add(team_post.writer.user)
-                
-          elif reported_type == 'team_post_comment':
-               reported_team_post_comment_pk = request.data.get('reported_team_post_comment', None)
-               team_post_comment = get_object_or_404(TeamPostComment, pk=reported_team_post_comment_pk)
-               
-               # abort if reported writer is reporter
-               if user.pk == team_post_comment.writer.user.pk:
-                    return Response({"detail": "user cannot report oneself"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-               
-               # block writer(user)
-               if block:
-                    user.blocked_users.add(team_post_comment.writer.user)
-          
-          # add user instance to request data
-          request.data['reporter'] = user.pk
-          
-          # create report
-          serializer = self.get_serializer(data=request.data)
-          serializer.is_valid(raise_exception=True)
-          report = serializer.save()
-          
-          return Response(serializer.data, status=status.HTTP_201_CREATED)
+class UserReviewReportCreateAPIView(ReportCreateAPIView):
+     serializer_class = UserReviewReportDetailSerializer
+
+class UserReviewCommentReportCreateAPIView(ReportCreateAPIView):
+     serializer_class = UserReviewCommentReportDetailSerializer
