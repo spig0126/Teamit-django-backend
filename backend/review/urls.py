f4e2a8fc517e35058f3aea034ddab6816f8e6524
spig0126 <spig0126@gmail.com>
2024-04-02 23:27:30 +0900

    👔 Refactor: view user reviews using user name in url
    - before: user pk
    - after: user name

--- backend/review/urls.py
+++ backend/review/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
      path("info/", UserReviewOptionsDetailAPIView.as_view()), 
      path("", UserReviewCreateAPIView.as_view(), name='create user review'), 
-     path("<int:pk>/", UserReviewListAPIView.as_view(), name='create, list user review'), 
+     path("<str:name>/", UserReviewListAPIView.as_view(), name='create, list user review'), 
      path("<int:pk>/", UserReviewRetrieveUpdateDestroyAPIView.as_view(), name='update, destroy user review'), 
      path("comments/", UserReviewCommentCreateAPIView.as_view(), name='create, list user review'), 
      path("comments/<int:pk>/", UserReviewCommentRetrieveUpdateDestroyAPIView.as_view(), name='update, destroy user review comment'), 
