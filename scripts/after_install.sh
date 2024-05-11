4d7c86e8d4ed72bf67497fec1625963bf34053f6
spig0126 <spig0126@gmail.com>
2024-05-11 20:50:08 +0900

    👷 CI/CD: create ci/cd system

---
+++ scripts/after_install.sh
@@ -0,0 +1,15 @@
+# kill any servers that may be running in the background 
+sudo pkill -f runserver
+
+cd /home/ubuntu/Teamit-django-backend-1/
+
+# activate virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# install requirements.txt
+pip install -r /home/ubuntu/acc-cicd-hands-on/requirements.txt
+
+# run server
+screen -d -m python3 manage.py runserver 0.0.0.0:8000 --settings=djangoProject.settings.prod
+
