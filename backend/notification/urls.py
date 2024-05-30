99ed3923b93c808b81801515081389ac37fe718e
spig0126 <spig0126@gmail.com>
2024-05-30 14:52:57 +0900

    ♻️ Refactor: remove unused api url

--- backend/notification/urls.py
+++ backend/notification/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-     path("team/<int:team_pk>/", TeamNotificationListAPIView.as_view()),
-     path("<int:team_pk>/", TeamNotificationAPIView.as_view()),
+     path("<int:team_pk>/", TeamNotificationListAPIView.as_view()),
      path("", NotificationListAPIView.as_view()),
      path("unread/status/", UnreadNotificationsStatusAPIView.as_view()),
      
