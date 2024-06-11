06b57cee933b786f8ce149d79118e374ca6b3236
spig0126 <spig0126@gmail.com>
2024-06-11 16:06:23 +0900

    Feat: new user event badge
    - create new badge
    - create signal for new badge
    - refactoring

--- backend/badge/models.py
+++ backend/badge/models.py
@@ -3,108 +3,104 @@ from django.core.files.storage import default_storage
 
 from user.models import User
 
+
 class BadgeLevels(models.IntegerChoices):
-     LEVEL_DEFAULT = 0, 'Level default'
-     LEVEL_ONE = 1, 'Level 1'
-     LEVEL_TWO = 2, 'Level 2'
-     LEVEL_THREE = 3, 'Level 3'
+    LEVEL_DEFAULT = 0, 'Level default'
+    LEVEL_ONE = 1, 'Level 1'
+    LEVEL_TWO = 2, 'Level 2'
+    LEVEL_THREE = 3, 'Level 3'
+
 
 class BadgeType(models.IntegerChoices):
-     ATTENDANCE = 0, 'attendance'
-     FRIENDSHIP = 1, 'friendship'
-     TEAM_PARTICIPANCE = 2, 'team_participance'
-     TEAM_POST = 3, 'team_post'
-     LIKED = 4, 'liked'
-     RECRUIT = 5, 'recruit'
-     TEAM_REFUSAL = 6, 'team_refusal'
-     USER_PROFILE = 7, 'user_profile'
-     TEAM_LEADER = 8, 'team_leader'
-     SHARED_PROFILE = 9, 'shared_profile'
-     REVIEW = 10, 'review'
+    ATTENDANCE = 0, 'attendance'
+    FRIENDSHIP = 1, 'friendship'
+    TEAM_PARTICIPANCE = 2, 'team_participance'
+    TEAM_POST = 3, 'team_post'
+    LIKED = 4, 'liked'
+    RECRUIT = 5, 'recruit'
+    TEAM_REFUSAL = 6, 'team_refusal'
+    USER_PROFILE = 7, 'user_profile'
+    TEAM_LEADER = 8, 'team_leader'
+    SHARED_PROFILE = 9, 'shared_profile'
+    EARLY_USER = 10, 'early_user'
+
 
 class Badge(models.Model):
-     user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE, related_name='badge')
-     attendance_cnt = models.PositiveIntegerField(default=0)
-     attendance_change = models.BooleanField(default=False)
-     friendship_level = models.IntegerField(choices=BadgeLevels.choices, default=BadgeLevels.LEVEL_DEFAULT)
-     friendship_change = models.BooleanField(default=False)
-     team_participance_cnt = models.PositiveIntegerField(default=0)
-     team_participance_change = models.BooleanField(default=False)
-     team_post_level = models.IntegerField(choices=BadgeLevels.choices, default=BadgeLevels.LEVEL_DEFAULT)
-     team_post_change = models.BooleanField(default=False)
-     liked_level = models.IntegerField(choices=BadgeLevels.choices, default=BadgeLevels.LEVEL_DEFAULT)
-     liked_change = models.BooleanField(default=False)
-     recruit_cnt = models.PositiveIntegerField(default=0)
-     recruit_change = models.BooleanField(default=False)
-     team_refusal_status = models.BooleanField(default=False)
-     team_refusal_change = models.BooleanField(default=False)
-     user_profile_status = models.BooleanField(default=False)
-     user_profile_change = models.BooleanField(default=False)
-     team_leader_status =  models.BooleanField(default=False)
-     team_leader_change = models.BooleanField(default=False)
-     shared_profile_status = models.BooleanField(default=False)
-     shared_profile_change = models.BooleanField(default=False)
-     review_status = models.BooleanField(default=False)
-     review_change = models.BooleanField(default=False)
-     
-     @property
-     def recruit_level(self):
-          if self.recruit_cnt >= 30:
-               return BadgeLevels.LEVEL_THREE
-          elif self.recruit_cnt >= 15:
-               return BadgeLevels.LEVEL_TWO
-          elif self.recruit_cnt >= 5:
-               return BadgeLevels.LEVEL_ONE
-          else:
-               return BadgeLevels.LEVEL_DEFAULT
-     
-     @property
-     def team_participance_level(self):
-          if self.team_participance_cnt >= 5:
-               return BadgeLevels.LEVEL_THREE
-          elif self.team_participance_cnt >= 3:
-               return BadgeLevels.LEVEL_TWO
-          elif self.team_participance_cnt >= 1:
-               return BadgeLevels.LEVEL_ONE
-          else:
-               return BadgeLevels.LEVEL_DEFAULT
-     
-     @property
-     def attendance_level(self):
-          if self.attendance_cnt >= 25:
-               return BadgeLevels.LEVEL_THREE
-          elif self.attendance_cnt >= 14:
-               return BadgeLevels.LEVEL_TWO
-          elif self.attendance_cnt >= 5:
-               return BadgeLevels.LEVEL_ONE
-          else:
-               return BadgeLevels.LEVEL_DEFAULT
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE, related_name='badge')
+    attendance_cnt = models.PositiveIntegerField(default=0)
+    attendance_change = models.BooleanField(default=False)
+    friendship_level = models.IntegerField(choices=BadgeLevels.choices, default=BadgeLevels.LEVEL_DEFAULT)
+    friendship_change = models.BooleanField(default=False)
+    team_participance_cnt = models.PositiveIntegerField(default=0)
+    team_participance_change = models.BooleanField(default=False)
+    team_post_level = models.IntegerField(choices=BadgeLevels.choices, default=BadgeLevels.LEVEL_DEFAULT)
+    team_post_change = models.BooleanField(default=False)
+    liked_level = models.IntegerField(choices=BadgeLevels.choices, default=BadgeLevels.LEVEL_DEFAULT)
+    liked_change = models.BooleanField(default=False)
+    recruit_cnt = models.PositiveIntegerField(default=0)
+    recruit_change = models.BooleanField(default=False)
+    team_refusal_status = models.BooleanField(default=False)
+    team_refusal_change = models.BooleanField(default=False)
+    user_profile_status = models.BooleanField(default=False)
+    user_profile_change = models.BooleanField(default=False)
+    team_leader_status = models.BooleanField(default=False)
+    team_leader_change = models.BooleanField(default=False)
+    shared_profile_status = models.BooleanField(default=False)
+    shared_profile_change = models.BooleanField(default=False)
+    early_user_status = models.BooleanField(default=False)
+    early_user_change = models.BooleanField(default=False)
 
-     @property
-     def shared_profile_level(self):
-          if self.shared_profile_cnt >= 30:
-               return BadgeLevels.LEVEL_THREE
-          elif self.shared_profile_cnt >= 20:
-               return BadgeLevels.LEVEL_TWO
-          elif self.shared_profile_cnt >= 10:
-               return BadgeLevels.LEVEL_ONE
-          else:
-               return BadgeLevels.LEVEL_DEFAULT
-     
-     @property
-     def latest_badge_images(self):
-          badge_types = [choice[1] for choice in BadgeType.choices]
-          img = []
-          for badge_type in badge_types:
-               try:
-                    badge_level = getattr(self, badge_type + '_level')
-               except AttributeError:
-                    try:
-                         badge_level = getattr(self, badge_type + '_status')
-                    except AttributeError:
-                         badge_level = getattr(self.__class__, badge_type + '_level').__get__(self)
-               if type(badge_level) is bool and badge_level:
-                    img.append(default_storage.url(f'badges/{badge_type}.png'))
-               elif badge_level:
-                    img.append(default_storage.url(f'badges/{badge_type}/{badge_level}.png'))
-          return img
+    @property
+    def recruit_level(self):
+        if self.recruit_cnt >= 30:
+            return BadgeLevels.LEVEL_THREE
+        elif self.recruit_cnt >= 15:
+            return BadgeLevels.LEVEL_TWO
+        elif self.recruit_cnt >= 5:
+            return BadgeLevels.LEVEL_ONE
+        else:
+            return BadgeLevels.LEVEL_DEFAULT
+
+    @property
+    def team_participance_level(self):
+        if self.team_participance_cnt >= 5:
+            return BadgeLevels.LEVEL_THREE
+        elif self.team_participance_cnt >= 3:
+            return BadgeLevels.LEVEL_TWO
+        elif self.team_participance_cnt >= 1:
+            return BadgeLevels.LEVEL_ONE
+        else:
+            return BadgeLevels.LEVEL_DEFAULT
+
+    @property
+    def attendance_level(self):
+        if self.attendance_cnt >= 25:
+            return BadgeLevels.LEVEL_THREE
+        elif self.attendance_cnt >= 14:
+            return BadgeLevels.LEVEL_TWO
+        elif self.attendance_cnt >= 5:
+            return BadgeLevels.LEVEL_ONE
+        else:
+            return BadgeLevels.LEVEL_DEFAULT
+
+    @property
+    def latest_badge_images(self):
+        badge_types = [choice[1] for choice in BadgeType.choices]
+        img = []
+        for badge_type in badge_types:
+            try:
+                badge_level = getattr(self, badge_type + '_level')
+            except AttributeError:
+                try:
+                    badge_level = getattr(self, badge_type + '_status')
+                except AttributeError:
+                    badge_level = getattr(self.__class__, badge_type + '_level').__get__(self)
+            if type(badge_level) is bool and badge_level:
+                img.append(default_storage.url(f'badges/{badge_type}.png'))
+            elif badge_level:
+                img.append(default_storage.url(f'badges/{badge_type}/{badge_level}.png'))
+        return img
+
+
+class UserCount(models.Model):
+    cnt = models.PositiveIntegerField(default=0)
--- backend/badge/signals.py
+++ backend/badge/signals.py
@@ -8,133 +8,161 @@ from post.models import TeamPost
 from .models import *
 from .utils import *
 
+
 @receiver(post_save, sender=User)
-def create_badge(sender, instance, created, **kwargs):
-     if created:
-          with transaction.atomic():
-               Badge.objects.create(user=instance)
+def create_badge_and_count_user(sender, instance, created, **kwargs):
+    if created:
+        with transaction.atomic():
+            badge = Badge.objects.create(user=instance)
+            user_cnt = UserCount.objects.first()
+            user_cnt.cnt += 1
+            user_cnt.save()
+            if user_cnt.cnt < 1000:
+                badge.early_user_status = True
+                badge.early_user_change = True
+                badge.save()
+
 
 @receiver(m2m_changed, sender=User.friends.through)
 def update_friendship_level(sender, instance, action, pk_set, **kwargs):
-     if action == 'post_add':
-          badge = instance.badge
-          if badge.friendship_level < 3:
-               friend_cnt = instance.friends.count()
-               before_level = badge.friendship_level
-               if friend_cnt >= 50:
-                    badge.friendship_level = 3
-               elif friend_cnt >= 30:
-                    badge.friendship_level = 2
-               elif friend_cnt >= 5:
-                    badge.friendship_level = 1
-               if before_level != badge.friendship_level:
-                    badge.friendship_change = True
-                    send_level_badge_fcm(instance.pk, 'friendship', badge.friendship_level)
-               badge.save()
+    if action == 'post_add':
+        badge = instance.badge
+        if badge.friendship_level < 3:
+            friend_cnt = instance.friends.count()
+            before_level = badge.friendship_level
+            if friend_cnt >= 50:
+                badge.friendship_level = BadgeLevels.LEVEL_THREE
+            elif friend_cnt >= 30:
+                badge.friendship_level = BadgeLevels.LEVEL_TWO
+            elif friend_cnt >= 5:
+                badge.friendship_level = BadgeLevels.LEVEL_ONE
+            if before_level != badge.friendship_level:
+                badge.friendship_change = True
+                send_level_badge_fcm(instance.pk, 'friendship', badge.friendship_level)
+            badge.save()
+
 
 @receiver(post_save, sender=TeamMembers)
 def update_team_participance_level(sender, instance, created, **kwargs):
-     if created:
-          user = instance.user
-          badge = user.badge
-          if badge.team_participance_level < 3:
-               before_level = badge.team_participance_level
-               badge.team_participance_cnt += 1
-               if before_level != badge.team_participance_level:
-                    badge.team_participance_change = True
-                    send_level_badge_fcm(user.pk, 'team_participance', badge.team_participance_level)
-               badge.save()
-               
+    if created:
+        user = instance.user
+        badge = user.badge
+        if badge.team_participance_level < 3:
+            before_level = badge.team_participance_level
+            badge.team_participance_cnt += 1
+            if before_level != badge.team_participance_level:
+                badge.team_participance_change = True
+                send_level_badge_fcm(user.pk, 'team_participance', badge.team_participance_level)
+            badge.save()
+
+
 @receiver(post_save, sender=TeamPost)
 def update_team_post_level(sender, instance, created, **kwargs):
-     if created:
-          user = instance.writer.user
-          badge = user.badge
-          if badge.team_post_level < 3:
-               before_level = badge.team_post_level
-               team_post_cnt = TeamPost.objects.filter(writer__user=user).count()
-               if team_post_cnt >= 30:
-                    badge.team_post_level = 3
-               elif team_post_cnt >= 10:
-                    badge.team_post_level = 2
-               elif team_post_cnt >= 5:
-                    badge.team_post_level = 1
-               if before_level != badge.team_post_level:
-                    badge.team_post_change = True
-                    send_level_badge_fcm(user.pk, 'team_post', badge.team_post_level)
-               badge.save()
+    if created:
+        user = instance.writer.user
+        badge = user.badge
+        if badge.team_post_level < 3:
+            before_level = badge.team_post_level
+            team_post_cnt = TeamPost.objects.filter(writer__user=user).count()
+            if team_post_cnt >= 30:
+                badge.team_post_level = 3
+            elif team_post_cnt >= 10:
+                badge.team_post_level = 2
+            elif team_post_cnt >= 5:
+                badge.team_post_level = 1
+            if before_level != badge.team_post_level:
+                badge.team_post_change = True
+                send_level_badge_fcm(user.pk, 'team_post', badge.team_post_level)
+            badge.save()
+
 
 @receiver(post_save, sender=TeamMembers)
 def update_recruit_level(sender, instance, created, **kwargs):
-     if created:
-          creator = instance.team.creator
-          badge = creator.badge
-          if badge.recruit_level < 3:
-               before_level = badge.recruit_level
-               badge.recruit_cnt += 1
-               if before_level != badge.recruit_level:
-                    badge.recruit_change = True
-                    send_level_badge_fcm(creator.pk, 'recruit', badge.recruit_level)
-               badge.save()
+    if created:
+        creator = instance.team.creator
+        badge = creator.badge
+
+        if creator == instance.user:
+            return
+        if badge.recruit_level >= 3:
+            return
+        before_level = badge.recruit_level
+        badge.recruit_cnt += 1
+        if before_level != badge.recruit_level:
+            badge.recruit_change = True
+            send_level_badge_fcm(creator.pk, 'recruit', badge.recruit_level)
+        badge.save()
+
 
 @receiver(post_save, sender=UserLikes)
 def update_liked_level(sender, instance, created, **kwargs):
-     liked_user = instance.to_user
-     badge = liked_user.badge
-     if badge.liked_level < 3:
-          before_level = badge.liked_level
-          liked_cnt = UserLikes.objects.filter(to_user=liked_user).count()
-          if liked_cnt >= 50:
-               badge.liked_level = 3
-          elif liked_cnt >= 30:
-               badge.liked_level = 2
-          elif liked_cnt >= 10:
-               badge.liked_level = 1
-          if before_level != badge.liked_level:
-               badge.liked_change = True
-               send_level_badge_fcm(liked_user.pk, 'liked', badge.liked_level)
-               
-          badge.save()
+    liked_user = instance.to_user
+    badge = liked_user.badge
+    if badge.liked_level >= 3:
+        return
+    before_level = badge.liked_level
+    liked_cnt = UserLikes.objects.filter(to_user=liked_user).count()
+    if liked_cnt >= 50:
+        badge.liked_level = BadgeLevels.LEVEL_THREE
+    elif liked_cnt >= 30:
+        badge.liked_level = BadgeLevels.LEVEL_TWO
+    elif liked_cnt >= 10:
+        badge.liked_level = BadgeLevels.LEVEL_ONE
+    if before_level != badge.liked_level:
+        badge.liked_change = True
+        send_level_badge_fcm(liked_user.pk, 'liked', badge.liked_level)
+        badge.save()
+
 
 @receiver(post_save, sender=TeamApplication)
 def update_team_refusal_status(sender, instance, created, **kwargs):
-     if not created:
-          user = instance.applicant
-          badge = user.badge
-          if not badge.team_refusal_status and not instance.accepted:
-               badge.team_refusal_status = True
-               badge.team_refusal_change = True
-               badge.save()
-               send_status_badge_fcm(user.pk, 'team_refusal')
+    if created:
+        return
+    user = instance.applicant
+    badge = user.badge
+    if badge.team_refusal_status or instance.accepted:
+        return
+    badge.team_refusal_status = True
+    badge.team_refusal_change = True
+    badge.save()
+    send_status_badge_fcm(user.pk, 'team_refusal')
+
 
 @receiver(post_save, sender=UserProfile)
 def update_user_profile_status(sender, instance, created, **kwargs):
-     if not created:
-          badge = instance.user.badge
-          if not badge.user_profile_status:
-               fields_to_check = [
-                    instance.education,
-                    instance.keywords,
-                    instance.tools,
-                    instance.experiences,
-                    instance.certificates,
-                    instance.links
-               ]
-               if not any(field == '' for field in fields_to_check):
-                    badge.user_profile_status = True
-                    badge.user_profile_change = True
-                    badge.save()
-                    send_status_badge_fcm(instance.user.pk, 'user_profile')
-                    
+    if created:
+        return
+    badge = instance.user.badge
+    if badge.user_profile_status:
+        return
+    str_fields_to_check = [
+        instance.education,
+        instance.keywords,
+        instance.tools,
+        instance.certificates,
+    ]
+    related_fields_to_check = [
+        instance.experiences,
+        instance.links
+    ]
+    if any(field == '' for field in str_fields_to_check) or \
+            any(not field.count() for field in related_fields_to_check):
+        return
+    badge.user_profile_status = True
+    badge.user_profile_change = True
+    badge.save()
+    send_status_badge_fcm(instance.user.pk, 'user_profile')
 
 
 @receiver(post_save, sender=Team)
 def update_team_leader_status(sender, instance, created, **kwargs):
-     if created:
-          user = instance.creator
-          badge = user.badge
-          if not badge.team_leader_status:
-               badge.team_leader_status = True
-               badge.team_leader_change = True
-               badge.save()
-               send_status_badge_fcm(user.pk, 'team_leader')
+    if not created:
+        return
+    user = instance.creator
+    badge = user.badge
+    if badge.team_leader_status:
+        return
+    badge.team_leader_status = True
+    badge.team_leader_change = True
+    badge.save()
+    send_status_badge_fcm(user.pk, 'team_leader')
