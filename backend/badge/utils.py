6f0e71cb69bfe0c4a38b6598da1e92de0dbc2aa9
spig0126 <spig0126@gmail.com>
2024-06-19 13:40:27 +0900

    Chore(db): remove friendship badge and send page in badge fcm

--- backend/badge/utils.py
+++ backend/badge/utils.py
@@ -6,6 +6,7 @@ def send_level_badge_fcm(user_pk, type, level):
      title = '배지 받아가세요!'
      body = '지금 바로 확인해보세요'
      data = {
+          'page': 'badge',
           'title': BADGE_TITLES[type],
           'level': str(level),
           'img': default_storage.url(f'badges/{type}/{level}.png')
@@ -16,6 +17,7 @@ def send_status_badge_fcm(user_pk, type):
      title = '배지 받아가세요!'
      body = '지금 바로 확인해보세요'
      data = {
+          'page': 'badge',
           'title': BADGE_TITLES[type],
           'level': '0',
           'img': default_storage.url(f'badges/{type}.png')
